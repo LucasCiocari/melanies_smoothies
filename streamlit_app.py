29876417194be59fa2e9b48d56da958f18584c25
Lucas Bertoglio Ciocari <lucasciocari@gmail.com>
2024-11-15 14:41:50 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,6 @@ st.write(
 )
 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-
 
 name_on_order = st.text_input("Name on Smoothie",)
 st.write("The name on your Smoothie will be", name_on_order)
@@ -49,3 +46,8 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' +name_on_order+ '!', icon='✅')
+
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
